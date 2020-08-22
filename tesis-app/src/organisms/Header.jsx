e8ab2e9e2659442c05f5737178376a61a564bd59
Rodson Vladimir <50309977+aymerod3009@users.noreply.github.com>
2020-08-22 18:34:33 -0500

    first login

--- tesis-app/src/organisms/Header.jsx
+++ tesis-app/src/organisms/Header.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { createRef } from 'react'
+import { NavLink } from 'react-router-dom'
+
+const menu = createRef()
+const toggleMenu = () => menu.current.classList.toggle('show')
 
 const Header = () => {
    return(
    <header className="main-header">
-       <div className="ed-grid">
+       <div className="ed-grid s-grid-5 lg-grid-4">
+        <div className="s-cols-4 lg-cols-1 s-cross-center">
+            <NavLink to="/">
+            <img src="http://miqueperu.com/img/me.png" alt="MDlogo" className="main-logo" />
+            </NavLink>
+        </div>
+        <div className="s-cols-1 lg-cols-3 s-cross-center s-main-end">
+            <nav className="main-menu" ref={menu}>
+                <ul>
+                    <li><NavLink exact to ="/">Inicio</NavLink></li>
+                    <li><NavLink to ="/login"> Salir</NavLink></li>
+                </ul>
+            </nav>
+            <div className="main-menu-toggle to-l"
+            
+           onClick={()=>toggleMenu()} >
 
+            </div>
+        </div>
        </div>
    </header>
    )
--- tesis-app/src/pages/login.jsx
+++ tesis-app/src/pages/login.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import Axios from 'axios'
+
+const authentication = e =>{
+    e.preventDefault()
+    const form = e.target 
+    
+    const data ={
+        "email": form.email.value,
+        "password": form.password.value
+    }
+   
+    Axios.post('3.230.230.245:5000/api/auth/login',data)
+    .then(r => console.log(r))
+    .catch(e => console.log(e))
+}
+
 
 const login = () => {
     return(
     <div className="ed-grid">
-            <h1>login </h1>
+        <div className="l-block"></div>
+            <div className="m-to-center m-60 lg-30">
+                <h1 className="center">
+                <img src="http://miqueperu.com/img/me.png" alt="MDlogo"  />
+                </h1>
+                <form onSubmit={authentication.bind()}>
+                <div className="form__item">
+                    <label htmlFor="email">
+                        Correo Electronico
+                        <input type="email" name="email" id="email" placeholder="Ingrese su e-mail" required/>
+                    </label>
+                </div>
+                <div className="form__item">
+                    <label htmlFor="password">
+                        Contraseña
+                        <input type="password" name="password" id="password" placeholder="Ingrese su Contraseña" required/>
+                    </label>
+                </div>
+                <div className="form_item">
+                    <input type="submit" className="button full" value="Iniciar Sesion"/>
+                </div>
+                </form>
+            </div>
         </div>
     )
 }

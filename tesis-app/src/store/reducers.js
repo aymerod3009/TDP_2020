2abdeb84cfd7ea5b0ccc83d3627aa05aed43bc6d
Rodson Vladimir <50309977+aymerod3009@users.noreply.github.com>
2020-08-21 21:07:20 -0500

    redux_s

---
+++ tesis-app/src/store/reducers.js
@@ -0,0 +1,22 @@
+export const postReducer = (state = {}, action) => {
+  return state
+}
+export const DeviceReducer = (state = {}, action) => {
+  return state
+}
+export const sadminReducer = (state = {}, action) => {
+  return state
+}
+export const DoctorReducer = (state = {}, action) => {
+  return state
+}
+export const PatientReducer = (state = {}, action) => {
+  return state
+}
+
+export const ClinicReducer = (state = {}, action) => {
+  return state
+}
+export const NotifyReducer = (state = {}, action) => {
+  return state
+}
--- tesis-app/src/store/store.js
+++ tesis-app/src/store/store.js
@@ -1,6 +1,16 @@
-import { createStore,combineReducers} from "redux"
+import { createStore,combineReducers,applyMiddleware} from "redux"
+import {composeWithDevTools} from "redux-devtools-extension"
+import thunk from "redux-thunk"
+import {postReducer,
+        DeviceReducer,
+        sadminReducer,
+        DoctorReducer,
+        PatientReducer,
+        ClinicReducer,
+        NotifyReducer} from "./reducers"
 
 export default createStore(
-    combineReducers()
+    combineReducers(postReducer,DeviceReducer,sadminReducer,DoctorReducer,PatientReducer,ClinicReducer,NotifyReducer),
+    composeWithDevTools(applyMiddleware(thunk))
 
 )
